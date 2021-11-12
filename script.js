def95f1c6f3540e8c675eb9fd0690fd404ba4c90
Pablo <pablogqueiroz@hotmail.com>
2021-11-11 22:03:28 -0300

    Acréscimo de funcionalidade do botão cancelar e voltar, e do incremento e decremento da quantidade

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let modalQt = 1;
 const s = (el) => document.querySelector(el);
 const st = (el) => document.querySelectorAll(el);
 
+
+//Listagem das Pizzas
 pizzaJson.map((item, index) => {
     let pizzaItem = s('.models .pizza-item').cloneNode(true);
 
@@ -12,25 +15,57 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
 
         s('.pizzaBig img').src = pizzaJson[key].img;
         s('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
-        s('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;        
+        s('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         s('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
-        st('pizzaInfo--size').forEach((size, sizeIndex)=>{
-            size.querySelector('span').innerHTML = "123";
-        })
+        s('.pizzaInfo--size.selected').classList.remove('selected')
+        st('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if (sizeIndex == 2) {
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
+
+        s('.pizzaInfo--qt').innerHTML = modalQt;
 
         s('.pizzaWindowArea').style.opacity = 0;
         s('.pizzaWindowArea').style.display = 'flex';
-        setTimeout(()=>{
+        setTimeout(() => {
             s('.pizzaWindowArea').style.opacity = 1;
         })
-    
+
     });
 
-
-
-
     s('.pizza-area').append(pizzaItem);
+});
+
+//Eventos do MODAL
+function closeModal() {
+    s('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+        s('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+}
+st('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+    item.addEventListener('click', closeModal);
+})
+
+s('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if (modalQt > 1) {
+        modalQt--;
+        s('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+});
+s('.pizzaInfo--qtmais').addEventListener('click', () => {
+    modalQt++;
+    s('.pizzaInfo--qt').innerHTML = modalQt;
+});
+st('.pizzaInfo--size').forEach((size, sizeIndex) => {
+    size.addEventListener('click', (e) => {
+        s('.pizzaInfo--size.selected').classList.remove('selected')
+        size.classList.add('selected');
+    })
 });
